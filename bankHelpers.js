7457a48fffbf42fcd63d64ff9d14265d893a3b61
Elijah <efroh10@gmail.com>
2021-05-04 14:50:36 -0700

    finsihed up

--- bankHelpers.js
+++ bankHelpers.js
@@ -1,31 +1,75 @@
-newUser.onsubmit = function(event)  {
-    event.preventDefault();
+window.onload = function(event){            //This whole section is actually unnecessary!!
+    let newUser = document.querySelector('#newUser');
     let user = document.querySelector('#user');
     let currency = document.querySelector('#currency');
     let description = document.querySelector('#description');
     let balance = document.querySelector('#balance');
-    let forbidden = ['!', '@', '#', '$', '%', '^', '&', '*']
+    let userNote = document.querySelector('#userNote');
+    let currencyNote = document.querySelector('#currencyNote');
+    let descriptionNote = document.querySelector('#descriptionNote');
+    let balanceNote = document.querySelector('#balanceNote');
 
-    if ((user.value.length < 8) || (user.value.includes('$'))){
-        user.classList.add('invalid');
-        return;
+
+    user.onblur = function(event){
+        if(!(/^([a-zA-Z0-9]{8,25})$/.test(user.value))){
+            user.classList.add('invalid');
+            userNote.classList.remove('hidden');
+        }
     }
-    else if ((balance.value < 50) || (isNaN(balance.value))){
-        balance.classList.add('invalid');
-        return;
+
+    user.onfocus = function(event){
+        user.classList.remove('invalid');
+        userNote.classList.add('hidden');
+    }   
+
+    currency.onblur = function(event){
+        if(currency.value == ''){
+            currency.classList.add('invalid');
+            currencyNote.classList.remove('hidden');
+        }
     }
-    else{
+
+    currency.onfocus = function(event){
+        currency.classList.remove('invalid');
+        currencyNote.classList.add('hidden');
+    }
+
+    description.onblur = function(event){
+        if(description.value == ''){
+            description.classList.add('invalid');
+            descriptionNote.classList.remove('hidden');
+        }
+    }
+
+    description.onfocus = function(event){
+        description.classList.remove('invalid');
+        descriptionNote.classList.add('hidden');
+    }
+
+    balance.onblur = function(event){
+        if(balance.value < 50){
+            balance.classList.add('invalid');
+            balanceNote.classList.remove('hidden');
+        }
+    }
+
+    balance.onfocus = function(event){
+        balance.classList.remove('invalid');
+        balanceNote.classList.add('hidden');
+    }
+// could have put additional verification in case onblur isn't triggered before submit
+    newUser.onsubmit = function(event){
+        event.preventDefault();
         fetch("http://localhost:5000/api/accounts", {
         "method": "POST",
         "headers": {"Content-Type": "application/json"},
-        "body": JSON.stringify({'user': user, 'description': description, 'currency': currency, 'balance': balance})
+        "body": JSON.stringify({'user': user.value, 'description': description.value, 'currency': currency.value, 'balance': balance.value})
         })
         .then(response => response.json())
         .then(data => console.log(data))
         .catch(err => console.error(err))
     }
 }
-
 
 // for the transaction page, i'll use js functions to change the innerhtml of the h1 to reflect change from login to transaction entry
 // ^ is there a better way than innerHTML?
--- bankTransactionHelpers.js
+++ bankTransactionHelpers.js
@@ -1,30 +1,64 @@
-login.onsubmit = function(event){
-    event.preventDefault();
-    let loginUser = document.querySelector('#loginUser').value;
+window.onload = function(event){
+    let loginUser = document.querySelector('#loginUser');
     let newTransaction = document.querySelector('#newTransaction');
     let login = document.querySelector('#login');
-    let heading = document.querySelector('h2');
-
-    newTransaction.classList.remove('hidden');
-    login.classList.add('hidden');
-    heading.innerHTML = 'New Transaction';
-    return loginUser;
-}
-
-newTransaction.onsubmit = function(event){
-    event.preventDefault();
+    let heading = document.querySelector('h2'); //this is the only line of this section that's actually necessary!!
     let date = document.querySelector('#date');
     let object = document.querySelector('#object');
     let amount = document.querySelector('#amount');
-    console.log(loginUser.value); 
+    let dateNote = document.querySelector('#dateNote');
+    let objectNote = document.querySelector('#objectNote');
+    let amountNote = document.querySelector('#amountNote');
 
-    if ((amount.value >= 0) || isNaN(amount.value)){
-        amount.classList.add('invalid');
-        return;
+
+    login.onsubmit = function(event){
+        event.preventDefault();
+        newTransaction.classList.remove('hidden');
+        login.classList.add('hidden');
+        heading.innerHTML = 'New Transaction';
+        return loginUser;
     }
-    else{
+
+    date.onblur = function(event){
+        if(date.value == ''){
+            date.classList.add('invalid');
+            dateNote.classList.remove('hidden');
+        }
+    }
+
+    date.onfocus = function(event){
+        date.classList.remove('invalid');
+        dateNote.classList.add('hidden');
+    }
+
+    object.onblur = function(event){
+        if(object.value == ''){
+            object.classList.add('invalid');
+            objectNote.classList.remove('hidden');
+        }
+    }
+
+    object.onfocus = function(event){
+        object.classList.remove('invalid');
+        objectNote.classList.add('hidden');
+    }
+
+    amount.onblur = function(event){
+        if(amount.value >= 0){
+            amount.classList.add('invalid');
+            amountNote.classList.remove('hidden');
+        }
+    }
+
+    amount.onfocus = function(event){
         amount.classList.remove('invalid');
-        fetch(`http://localhost:5000/api/accounts/${loginUser.value}/transactions`, { //why do i have to add .value when i already did l3?
+        amountNote.classList.add('hidden');
+        }
+// could have put additional verification in case onblur isn't triggered before submit
+    newTransaction.onsubmit = function(event){
+        event.preventDefault();
+
+        fetch(`http://localhost:5000/api/accounts/${loginUser.value}/transactions`, {
         "method": "POST",
         "headers": {"Content-Type": "application/json"},
         "body": JSON.stringify({'date': date.value, 'object': object.value, 'amount': amount.value})
